9e85803bba3a5d8f251295a94835250f71d0d6ce
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-05 02:51:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function currentLine(deliLine){
     textReturn = textReturn + ': '
     for (let i=0;i<deliLine.length;i++){
       textReturn = textReturn + (i+1) + '. ' + deliLine[i]
-      if (i!==deliLine.length) {
+      if (i!==deliLine.length-1) {
         textReturn = textReturn + ', '
       }
     }
